f2e13c38624df09d9a0b374074d009cbd49b2704
Ziyaad-Labaran <Ziyaad.zalabaran@gmail.com>
2025-07-03 21:52:24 +1200

    gsap.fromTo

--- script.js
+++ script.js
@@ -7,10 +7,23 @@ gsap.to(".spongebob",{
     x: 400,
 })
 
-gsap.to(".football",{
+gsap.to('.football',{
     scale:2,
     duration:1,
-    repeat:-1,
+    // repeat:-1,
     yoyo:true,
-    
+
+    onComplete: ()=>{
+     gsap.fromTo('.football',{
+        x:400,
+        y:200,
+     },{
+        x:100,
+        y:300,
+        repeat:-1,
+        yoyo:true,
+     })
+    }
+        
+
 })
